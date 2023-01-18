9fa804797cd63979b4d81b9abe8e842f9ed03644
Serena-Dong <serenadong3159@icloud.com>
2023-01-18 17:19:22 +0100

    vue and axios

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then(function(response){
-    const result = response.data.response;
-    console.log(result);
-});
+const app = Vue.createApp({
+    data(){
+        return{
+            mail: ''
+        }
+    },
+    mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+        this.mail = response.data.response;
+        console.log(this.mail);
+        });
+
+    }
+})
+
+app.mount('#root');
